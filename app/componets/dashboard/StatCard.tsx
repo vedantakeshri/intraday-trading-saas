3b21ecdc22f53eaba039e8f4e82bb83b72bbc620
krishna soni <110759309+krishnaSONI011@users.noreply.github.com>
2026-03-31 15:41:47 +0530

    Update StatCard.tsx

--- app/componets/dashboard/StatCard.tsx
+++ app/componets/dashboard/StatCard.tsx
@@ -1,25 +1,3 @@
-// export default function StatCard({
-//   title,
-//   value,
-//   positive,
-// }: {
-//   title: string;
-//   value: string;
-//   positive?: boolean;
-// }) {
-//   return (
-//     <div className="bg-gray-900 border border-gray-800 rounded-xl p-4">
-//       <p className="text-sm text-gray-400">{title}</p>
-//       <p className={`text-xl font-bold ${positive ? "text-green-400" : ""}`}>
-//         {value}
-//       </p>
-//     </div>
-//   );
-// }
-
-
-
-
 
 
 import {
@@ -51,7 +29,8 @@ export default function StatCard({
   positive?: boolean;
   type?: string;
 }) {
-  const Icon = iconMap[type] || BarChart3;
+  // const Icon = iconMap[type] || BarChart3;
+  const Icon = (type && iconMap[type]) || BarChart3;
 
   return (
     <div className="bg-gray-900 border border-gray-800 rounded-xl p-4 flex justify-between items-center hover:border-gray-700 transition">
